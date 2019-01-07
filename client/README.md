f3268df9f94f532a8b90e545b5d3725d2ff9ff7e
jose31canizar <jose.canizares@colorado.edu>
2019-01-07 16:42:09 +0100

    write unit tests for reactive dashboard

--- client/README.md
+++ client/README.md
@@ -4,17 +4,21 @@ Created by José Cañizares
 
 In the project directory, you can run:
 
-### `npm start`
+### ``
 
-This launches the project in dev mode for other developers to change the code and immediately see changes.
+### `npm start && cd ../server && yd`
+
+This launches the project in dev mode for other developers to change the code and immediately see changes. This runs both the client and the fake server.
 
 ### `npm test`
 
 Launches the test runner in the interactive watch mode.
 
+### `CI=true npm test`
+
+Runs all tests once without watching for live changes.
+
 ### `npm run build`
 
 This builds the project for deployment.
-
-## Deployment
 
--- client/src/components/reactive-dashboard/index.test.js
+++ client/src/components/reactive-dashboard/index.test.js
@@ -7,26 +7,34 @@ import ReactiveDashboard from "./index";
 */
 
 describe("ReactiveDashboard", () => {
-  describe("when window width is less than threshold", () => {
+  describe("when window width is < than threshold", () => {
     it("should render graphs within slider", () => {
       const wrapper = mount(
-        <LoadingIndicator isLoading={false}>
-          <div>graphs</div>
-        </LoadingIndicator>
+        <ReactiveDashboard
+          width={790}
+          threshold={810}
+          render={() => <div>graphs</div>}
+        />
       );
-      expect(wrapper.html()).toEqual("<div>graphs</div>");
+      expect(wrapper.html()).not.toEqual(
+        '<div class="dashboard"><div>graphs</div></div>'
+      );
       wrapper.unmount();
     });
   });
 
-  describe("when window width is greater than threshold", () => {
+  describe("when window width is > than threshold", () => {
     it("should render graphs without slider", () => {
       const wrapper = mount(
-        <LoadingIndicator isLoading={false}>
-          <div>graphs</div>
-        </LoadingIndicator>
+        <ReactiveDashboard
+          width={810}
+          threshold={790}
+          render={() => <div>graphs</div>}
+        />
       );
-      expect(wrapper.html()).toEqual("<div>graphs</div>");
+      expect(wrapper.html()).toEqual(
+        '<div class="dashboard"><div>graphs</div></div>'
+      );
       wrapper.unmount();
     });
   });
